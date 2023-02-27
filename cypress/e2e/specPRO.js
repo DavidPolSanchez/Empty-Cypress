a9221056e6d9abf3aeffaa854ea9fda49b6324da
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2023-02-27 16:42:23 +0100

    example of a form and change the first test to PRO

---
+++ cypress/e2e/specPRO.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://pro.boehringer-ingelheim.com/')
+    //interaction with elements
+    cy.get('.it-eprivacy-banner-btn--primary').click()
+
+    cy.get('[title="Products"]').click()
+    //!!asertion
+    cy.url().should('eq','https://pro.boehringer-ingelheim.com/products')
+  })
+})
