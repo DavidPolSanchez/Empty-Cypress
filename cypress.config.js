79b2a48fd60db36ab68afbf7107d84e10ea76e6e
DavidPolSanchez <davidpolsanchezm98@gmail.com>
2023-02-27 11:35:22 +0100

    add configuration for specs

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight:1080,
+    viewportWidth:1920,
+    specPattern: 'cypress/e2e/**/*.js',
+    excludeSpecPattern:['**/1-getting-started/*','**/2-advanced-examples/*'],
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
+
